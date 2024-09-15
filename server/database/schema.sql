13c9fe78702ce8deb8b76a37f0f22b3264954804
samih ringuet <samrin2@proton.me>
2024-09-15 11:01:52 +0200

    :art: new table announce with fake data created

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -9,7 +9,23 @@ create table user (
   postal_code INT NOT NULL
 );
 
+create table announce (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  search_title VARCHAR(155) NOT NULL,
+  user_type ENUM("musicien", "groupe") NOT NULL,
+  instrument VARCHAR(155) NOT NULL,
+  location VARCHAR(155) NOT NULL,
+  description TEXT NOT NULL,
+  name VARCHAR(55) NOT NULL
+);
+
+
 INSERT INTO user (user_type, mail, password, first_name, last_name, country, postal_code)
 VALUES ("musicien", "ramon@gmail.com", "plataoplomo", "ramon", "sanchez", "spain", 45432),
 ("groupe", "lestaraudeurs@live.fr", "dzijofzhf", "jean-luc", "delastreet", "france", 20240),
 ("musicien", "beber@hotmail.com", "carbocream", "bernard", "grodar", "france", 34654);
+
+INSERT INTO announce (search_title, user_type, instrument, location, description, name)
+VALUES ("LF GUITAR HERO", "groupe", "guitare electrique", "nouillorc", "Nous recherchons un guitariste pour notre groupe , si possible qui aime faire hurler son materiel et s'arracher les phalanges", "POO ON SYSTEM"),
+("à la recherche d'un batteur", "groupe", "batterie", "los argeles", "si tu aimes tabasser ta batterie et uniquement ta batterie, tu es le bienvenue", "kawapunka"),
+("seek groupe latino", "musicien", "guitar", "lens", "chaud et dispo pour faire danser de la latine enervé", "johnny gimenez");
