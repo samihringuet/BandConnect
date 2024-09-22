7636862dab8955c699ced907f79009765131efca
samih ringuet <samrin2@proton.me>
2024-09-22 17:13:56 +0200

    :art: created homepage.jsx , import and rout on main , homepage css created too

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,20 @@
 export default function HomePage() {
-  return <h1>caca</h1>;
+  return (
+    <div className="homepagetotal">
+      <h1 className="thetitle">
+        <span>Band</span>⚡Connect
+      </h1>
+      <h2 className="saveurshow">SAVE YOUR SHOW</h2>
+      <h2 className="findurhero">FIND YOUR HERO</h2>
+      <div className="rockbox">
+        <div className="rockoverlay">
+          <h2>MENU</h2>
+          <p className="thunderclood">
+            the Thundercloud javagods saturday night
+          </p>
+          <p className="mulhouse">on mulhouse sunset drive ☢</p>
+        </div>
+      </div>
+    </div>
+  );
 }
